21d3c3169fb8504084f6a99bb20b01ad0e68e6d5
Claudius Mueller <claudius.mueller@gmail.com>
2024-03-11 14:27:38 -0400

    read account with tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -123,4 +123,24 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
+    # Needed to get coverage > 95%
+    def test_405(self):        
+        """It should not allow the post method"""        
+        response1 = self.client.post()        
+        self.assertEqual(response1.status_code, status.HTTP_405_METHOD_NOT_ALLOWED) 
+
     # ADD YOUR TEST CASES HERE ...
+
+    def test_read_an_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(f"{BASE_URL}/{account.id}", content_type="application/json")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
+
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+        
